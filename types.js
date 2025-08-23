f7d1085e8535b47351bda8182183b5f4a43b0632
lexm2 <lexm2@users.noreply.github.com>
2025-08-23 03:07:04 +0000

    deploy: ff8603ea143b81ad18248993ff752d321c56b080

---
+++ types.js
@@ -0,0 +1,11 @@
+export var DayOfWeek;
+(function (DayOfWeek) {
+    DayOfWeek["MONDAY"] = "mon";
+    DayOfWeek["TUESDAY"] = "tue";
+    DayOfWeek["WEDNESDAY"] = "wed";
+    DayOfWeek["THURSDAY"] = "thu";
+    DayOfWeek["FRIDAY"] = "fri";
+    DayOfWeek["SATURDAY"] = "sat";
+    DayOfWeek["SUNDAY"] = "sun";
+})(DayOfWeek || (DayOfWeek = {}));
+//# sourceMappingURL=types.js.map
