4aaf5bd70dbc66baa0611b9868e4082c93648eec
Lex <lex.k.moulton@gmail.com>
2025-10-31 10:09:11 -0400

    Icons

--- src/ui/utils/ProgressiveRenderer.ts
+++ src/ui/utils/ProgressiveRenderer.ts
@@ -192,7 +192,7 @@ export class ProgressiveRenderer {
                             <div class="course-details">
                                 <div class="course-name">
                                     ${course.name}
-                                    ${hasWarning ? '<span class="warning-icon" title="All sections full">⚠</span>' : ''}
+                                    ${hasWarning ? '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="warning-icon" title="All sections full"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm.01 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" /></svg>' : ''}
                                 </div>
                                 <div class="course-sections">
                                     ${course.sections.map(section => {
@@ -321,7 +321,7 @@ export class ProgressiveRenderer {
                         </div>
                         <div class="course-title">
                             ${course.name}
-                            ${hasWarning ? '<span class="warning-icon" title="All sections full">⚠</span>' : ''}
+                            ${hasWarning ? '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="warning-icon" title="All sections full"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm.01 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" /></svg>' : ''}
                         </div>
                         <div class="course-info">
                             <div class="course-info-top">
